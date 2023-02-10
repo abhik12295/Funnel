8a19c506ad0789cf07795f7edcb454743805fc1a
Abhishek Kumar <abhik12295@gmail.com>
2023-02-10 13:26:24 -0600

    updated the view models

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-#view we have most coding part -> views file in same dir
+# view we have most coding part -> views file in same dir
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
 ]
